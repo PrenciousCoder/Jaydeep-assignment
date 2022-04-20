5e8889de1383574b828d7456d86750bb50bbef40
Prencious Coder <devansh.panchal03@gmail.com>
2022-04-20 14:01:52 +0200

    Update Question17.py

--- Question17.py
+++ Question17.py
@@ -1,7 +1,13 @@
 # palindrome string
 #Question5
-string=input("enter a string: ")
-if string==string[::-1]:
+n=int(input())
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
     print(True)
 else:
     print(False)
