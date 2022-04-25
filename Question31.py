0645d88b260f8e52d3fbad3e60f53af2721886ef
Prencious Coder <devansh.panchal03@gmail.com>
2022-04-25 18:49:17 +0200

    Add files via upload

--- Question31.py
+++ Question31.py
@@ -3,19 +3,24 @@ num2=int(input())
 
 count_iterations=0
 cond=True
-while cond:
-    if num2>num1:
-        num2=num2-num1
-        count_iterations=count_iterations+1
-        if num2==0:
-            cond=False
+if num1==0 or num2==0:
+    print(None)
+else:
+    
+    while cond:
+        if num2>num1:
+            num2=num2-num1
+            count_iterations=count_iterations+1
+            if num2==0:
+                cond=False
+            else:
+                continue
         else:
-            continue
-    else:
-        num1=num1-num2
-        count_iterations=count_iterations+1
-        if num1==0:
-            cond=False
-        else:
-            continue
+            num1=num1-num2
+            count_iterations=count_iterations+1
+            if num1==0:
+                cond=False
+            else:
+                continue
+
 print(count_iterations)
