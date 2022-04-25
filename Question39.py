41e9794ae35731bfc87d0dab9d5e82b502d2b0e8
Prencious Coder <devansh.panchal03@gmail.com>
2022-04-25 20:04:34 +0200

    Add files via upload

--- Question39.py
+++ Question39.py
@@ -1,8 +1,12 @@
 
-
-
 import math
 a=int(input())
 b=int(input())
+def gcd(num1,num2):
+    if (num2==0):
+        return num1
+    else:
+        return gcd(num2, num1%num2)
 
-print(math.gcd(a,b))
+print(gcd(a,b))
+
