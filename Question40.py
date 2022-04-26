6930eaddcb43caea5c6b4d2dfc6646bc2e1a2515
Prencious Coder <devansh.panchal03@gmail.com>
2022-04-26 10:46:59 +0200

    Add files via upload

---
+++ Question40.py
@@ -0,0 +1,9 @@
+def rev_string(inp):
+    list_string=list(inp)
+    #print(list_string)
+
+    reverse_list=inp[::-1]
+    #print(reverse_list)
+    return reverse_list
+
+rev_string(inp=input())  
