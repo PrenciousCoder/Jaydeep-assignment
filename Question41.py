e0d05bcd0fc7445951d0b9613f72028e364aaa09
Prencious Coder <devansh.panchal03@gmail.com>
2022-05-04 22:02:52 +0200

    Add files via upload

--- Question41.py
+++ Question41.py
@@ -1,6 +1,16 @@
 def intersection(list1,list2):
+    print
     return list(set(list1) & set(list2))
 
 list1=[]
+
+temp=0
+for i in range(0,5):
+    temp=int(input("enter elements: "))
+    list1.append(temp)
 list2=[]
+for j in range(0,5):
+    temp+int(input("enter the elemnts: "))
+    list2.append(temp)
+    
 intersection(list1,list2)
