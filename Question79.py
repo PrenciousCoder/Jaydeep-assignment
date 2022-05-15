8c0a30dc18aac4552dd391062e0cf3d7c9ef6852
Prencious Coder <devansh.panchal03@gmail.com>
2022-05-15 11:28:00 +0200

    Add files via upload

--- Question79.py
+++ Question79.py
@@ -1,10 +1,17 @@
-n=int(input("Enter order of matrix: "))
-temp=0
 matrix=[]
-for j in range(0,(n*n)):
-    temp=int(input("Enter the elements: "))
-    matrix.append(temp)
+
+n=int(input("enter order of matrix"))
+row=n
+col=n
+print("Enter the matrix row wise")
+for i in range(row):
+    a=[]
+    for j in range(col):
+        a.append(int(input("Enter the elements")))
+    matrix.append(a)
 print(matrix)
+#correct
+
 
 def absDiffDiagonal(matrix,n):
     dia1=0
@@ -18,6 +25,8 @@ def absDiffDiagonal(matrix,n):
 
             if (i==n-j-1):
                 dia2+=matrix[i][j]
+    print(dia1)
+    print(dia2)
 
     return abs(dia1-dia2)
 
