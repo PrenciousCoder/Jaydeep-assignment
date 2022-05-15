a7b11dd33633d6738d0c2d282a7f7bf882cc559e
Prencious Coder <devansh.panchal03@gmail.com>
2022-05-15 18:41:53 +0200

    Add files via upload

--- Question64.py
+++ Question64.py
@@ -1,12 +1,23 @@
 #Question 11
 List=[]
+n=int(input("enter order of matrix"))
+row=n
+col=n
+print("Enter the matrix row wise")
+for i in range(row):
+    a=[]
+    for j in range(col):
+        a.append(int(input("Enter the elements")))
+    List.append(a)
+print(List)
 flat=[]
 def flatlist(List):
     for sublist in List:
         for arr in sublist:
             flat.append(arr)
 
-print(flat)
+    print(flat)
+flatlist(List)
 
 
 
